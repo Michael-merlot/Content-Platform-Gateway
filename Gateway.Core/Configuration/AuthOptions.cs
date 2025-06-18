198beb19f4a63dbe558e0bf1861ca3cb92aacf9d
Egor Boyarkin <egrsfn@gmail.com>
2025-06-18 23:08:50 +0300

    refactor(auth): remove redundant files and reconsider options layer
    Also moves IAuthenticationService DI registration to an appropriate
    place.

--- Gateway.Core/Configuration/AuthOptions.cs
+++ Gateway.Core/Configuration/AuthOptions.cs
@@ -1,11 +1,8 @@
-namespace Gateway.Api.Options;
+namespace Gateway.Core.Configuration;
 
 /// <summary>Authentication options.</summary>
 public class AuthOptions
 {
-    /// <summary>Public key in PEM format to verify JWTs.</summary>
-    public string PublicKeyPem { get; set; } = String.Empty;
-
     /// <summary>Authority to fetch JWKS from.</summary>
     public string Authority { get; set; } = String.Empty;
 }
